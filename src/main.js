d989a0c70c6ff05f9298bb80fb6a2b58d2dc41bd
jairoblatt <jairoblattc@gmail.com>
2020-06-13 10:33:43 -0300

    app.vue

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import store from './store'
 import router from './router'
 import i18n from './plugins/i18n'
-import App from './components/App'
+import App from './App'
 
 import './plugins'
 import './components'
